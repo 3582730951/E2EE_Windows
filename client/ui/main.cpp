cf986865eba888ebc8c19dab73669d8fe2028af3
星河旧梦 <65857043+3582730951@users.noreply.github.com>
2025-12-08 20:47:33 +0800

    ui: enforce local qml when present

--- client/ui/main.cpp
+++ client/ui/main.cpp
@@ -15,12 +15,12 @@ int main(int argc, char* argv[]) {
   engine.rootContext()->setContextProperty("clientBridge", &client);
   const QString appDir = QCoreApplication::applicationDirPath();
   const QString localMain = appDir + "/qml/Main.qml";
-  if (QFile::exists(localMain)) {
+  const bool hasLocal = QFile::exists(localMain);
+  if (hasLocal) {
     engine.addImportPath(appDir + "/qml");
   }
-  const QUrl url = QFile::exists(localMain)
-                       ? QUrl::fromLocalFile(localMain)
-                       : QUrl(QStringLiteral("qrc:/qt/qml/mi/e2ee/ui/Main.qml"));
+  const QUrl url = hasLocal ? QUrl::fromLocalFile(localMain)
+                            : QUrl(QStringLiteral("qrc:/qt/qml/mi/e2ee/ui/Main.qml"));
   QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
                    [url](QObject* obj, const QUrl& objUrl) {
                      if (!obj && url == objUrl) {
