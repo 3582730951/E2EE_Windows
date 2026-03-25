01c768019cca97668ba95236546f7f539fed2619
3582730951 <3582730951@qq.com>
2026-03-25 18:34:34 +0000

    fix: remove ios root geometry constraints

--- ios_root_app/RootAuthApp/RootAuthAppApp.swift
+++ ios_root_app/RootAuthApp/RootAuthAppApp.swift
@@ -4,16 +4,7 @@ import SwiftUI
 struct RootAuthAppApp: App {
     var body: some Scene {
         WindowGroup {
-            GeometryReader { proxy in
-                ZStack {
-                    SecureSceneBackground()
-
-                    AppShell()
-                        .frame(width: proxy.size.width, height: proxy.size.height)
-                }
-                .frame(width: proxy.size.width, height: proxy.size.height)
-                .background(SecurePalette.backgroundBottom)
-            }
+            AppShell()
         }
     }
 }
